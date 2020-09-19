0d7e119cb350ca1dc4cf597c8d77a65ece2cec45
AshwiniPoonacha <ashwini.poonacha@gmail.com>
2020-09-19 18:21:38 +0530

    New framework

---
+++ com.pageobjectModel/src/test/resources/LoginPageTest.java
@@ -0,0 +1,55 @@
+
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.Base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase {
+	LoginPage loginpage;
+	HomePage homepage;
+	
+	public LoginPageTest(){ 
+		super();
+	}
+	
+	
+	@BeforeMethod
+	public void setup(){
+		initialise();
+		loginpage=new LoginPage();
+	}
+	
+	
+	@Test(priority=1)
+	public void pagetitletest(){
+		String title=loginpage.validatepagetitle();
+		Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.");
+	}
+	
+	
+	@Test(priority=2)
+	public void CRMlogoimgtest(){
+		boolean yn=loginpage.crmimg();
+		Assert.assertEquals(true, yn);
+		
+	}
+	
+	@Test(priority=3)
+	public void logintest(){
+		homepage=loginpage.login(prop.getProperty("Username"), prop.getProperty("Password"));
+	}
+	
+	
+	
+	@AfterMethod
+	public void teardown(){
+		driver.quit();
+	}
+	
+
+}
